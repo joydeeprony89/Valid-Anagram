14a1601d89b29f131979993b5378608a60efeb7b
Jaydeep Shil <ED8A@eurofins.com>
2021-06-10 22:01:13 +0530

    Add project files.

---
+++ Valid Anagram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Valid_Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "anagram", t = "nagaram";
+            Console.WriteLine(IsAnagram(s, t));
+        }
+
+        static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            int[] hash = new int[26];
+
+            foreach (char c in s)
+                hash[c - 'a']++;
+
+            foreach (char c in t)
+            {
+                hash[c - 'a']--;
+                if (hash[c - 'a'] < 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
